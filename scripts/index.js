f831ef01699a4dc4dd13ef4ea33f8daaf90fadc0
Artlokos <artlokos@yandex.ru>
2024-03-05 00:41:52 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
 // @todo: Темплейт карточки
 
+
 // @todo: DOM узлы
+let addButton = document.querySelector('.profile__add-button');
+let pageSection = document.querySelector('.page__section');
+let placesList = document.querySelector('.places__list');
+let delButton = document.querySelector('card__delete-button');
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function addCard (init) {
+    let cardTemplate = document.querySelector('#card-template').content;
+    let nextCard = cardTemplate.querySelector('.card').cloneNode(true);
 
+    nextCard.querySelector('.card__title').textContent = init.name;
+    nextCard.querySelector('.card__image').src = init.link;
+    nextCard.querySelector('.card__image').alt = `Изображение места ${init.name}`;
+    return nextCard;
+};
+
+// @todo: Функция удаления карточки
+function del_card(cardToRemove) {
+}
 // @todo: Вывести карточки на страницу
+
+addButton.addEventListener('click', 
+function() 
+{
+    initialCards.forEach(init => 
+        {
+            console.log(addCard(init));
+            placesList.append(addCard(init));
+        });
+});
+
+delButton.addEventListener('click', function(){
+    let currentCard = "q";
+    // currentCard.remove();
+    console.log(currentCard);
+});
+

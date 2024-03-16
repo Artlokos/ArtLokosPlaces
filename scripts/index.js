0aff0fcfbf032e4e9bff69c09ac9777cfcb7c915
Artlokos <artlokos@yandex.ru>
2024-03-16 20:15:36 +0300

    without del

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,35 @@
 // @todo: Темплейт карточки
-
-
+let cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 let addButton = document.querySelector('.profile__add-button');
-let pageSection = document.querySelector('.page__section');
 let placesList = document.querySelector('.places__list');
-let delButton = document.querySelector('card__delete-button');
 
 // @todo: Функция создания карточки
 
-function addCard (init) {
-    let cardTemplate = document.querySelector('#card-template').content;
+function addCard (init, del_card) {
     let nextCard = cardTemplate.querySelector('.card').cloneNode(true);
 
     nextCard.querySelector('.card__title').textContent = init.name;
     nextCard.querySelector('.card__image').src = init.link;
     nextCard.querySelector('.card__image').alt = `Изображение места ${init.name}`;
+    nextCard.querySelector('.card__delete-button').addEventListener('click',function(){
+        del_card(nextCard);
+    // nextCard.querySelector('.card__delete-button').addEventListener('click', function(evt){
+    //     let cardToDelete = evt.target.closest('.card');
+    //     cardToDelete.remove();
+    });
     return nextCard;
 };
 
 // @todo: Функция удаления карточки
-function del_card(cardToRemove) {
+function del_card(evt) {
+    let cardToDelete = evt.target.closest('.card');
+    cardToDelete.remove();
 }
 // @todo: Вывести карточки на страницу
 
-addButton.addEventListener('click', 
-function() 
-{
-    initialCards.forEach(init => 
-        {
-            console.log(addCard(init));
-            placesList.append(addCard(init));
-        });
+addButton.addEventListener('click', function() {    
+    initialCards.forEach (init => {
+        placesList.append(addCard(init));
+    });
 });
-
-delButton.addEventListener('click', function(){
-    let currentCard = "q";
-    // currentCard.remove();
-    console.log(currentCard);
-});
-

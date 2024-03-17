347537c0865ed32cc20af44243a4f236a3dff978
ArtLokos <artlokos@yandex.ru>
2024-03-17 17:19:18 +1000

    afterFirstCheck

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,26 @@
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 const addButton = document.querySelector(".profile__add-button");
-let placesList = document.querySelector(".places__list");
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-function addCard(initCards) {
+function createCard(DataForCardFromArray, handleDelete) {
   let nextCard = cardTemplate.querySelector(".card").cloneNode(true);
   let delButton = nextCard.querySelector(".card__delete-button");
-  nextCard.querySelector(".card__title").textContent = initCards.name;
-  nextCard.querySelector(".card__image").src = initCards.link;
-  nextCard.querySelector(".card__image").alt = `Изображение места ${initCards.name}`;
-  delButton.addEventListener("click", function () {
-    handleDelete(nextCard);
-  });
+  const cardTitle = nextCard.querySelector(".card__title");
+  const cardImage = nextCard.querySelector(".card__image");
+  cardTitle.textContent = DataForCardFromArray.name;
+  cardImage.src = DataForCardFromArray.link;
+  cardImage.alt = `Изображение места ${DataForCardFromArray.name}`;
+  delButton.addEventListener("click", () => handleDelete(nextCard));
   return nextCard;
 }
 
 // @todo: Функция удаления карточки
-function handleDelete(cardToDelete) {
-  cardToDelete.remove();
-}
+const handleDelete = (cardToDelete) => cardToDelete.remove();
 // @todo: Вывести карточки на страницу
 
 addButton.addEventListener("click", function () {
-  initialCards.forEach((initCards) => {
-    placesList.append(addCard(initCards));
-  });
+  initialCards.forEach((DataForCardFromArray) => placesList.append(createCard(DataForCardFromArray, handleDelete)));
 });

c97c425bf36b95a33405276eb1dd8a17ca97094a
ArtLokos <artlokos@yandex.ru>
2024-04-13 01:39:25 +1000

    12042024

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -43,9 +43,9 @@ const popup = document.querySelector('.popup');
 const popupTypeNewCard = document.querySelector('.popup_type_new-card');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popupTypeImage = document.querySelector('.popup_type_image');
-const popupCloseButton = document.querySelector('.popup__close');
 const nameFromHeader = document.querySelector('.profile__title');
 const jobFromHeader = document.querySelector('.profile__description');
+
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
@@ -54,8 +54,7 @@ const addButton = document.querySelector(".profile__add-button");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const placesList = document.querySelector(".places__list");
 
-document.removeEventListener('keydown', closeWithEsc);
-// @todo: Функция создания карточки
+initialCards.forEach((cardData) => placesList.append(createCard(cardData, handleDelete)));
 
 function createCard(cardData, handleDelete) {
   const nextCard = cardTemplate.querySelector(".card").cloneNode(true);
@@ -73,66 +72,86 @@ function createCard(cardData, handleDelete) {
 const handleDelete = (cardToDelete) => cardToDelete.remove();
 // @todo: Вывести карточки на страницу
 
+addButton.addEventListener("dblclick", function () {
+  
+});
+
 addButton.addEventListener("click", function () {
-  initialCards.forEach((cardData) => placesList.append(createCard(cardData, handleDelete)));
-  openPopup(popupTypeNewCard,'popup_type_new-card');
+  openPopupNewCard(popupTypeNewCard);
 });
 
 function openPopup(elem) {
   document.addEventListener('keydown', closeWithEsc);
+  const popupContent = elem.querySelector('.popup__content')
+  const formPopup = elem.querySelector('.popup__form');
+  const popupCloseButton = popupContent.querySelector('.popup__close');
+  const placeForNameToInput = formPopup.querySelector('.popup__input_type_name');
+  const placeForJobToInput = formPopup.querySelector('.popup__input_type_description');
+  placeForNameToInput.value = nameFromHeader.textContent;
+  placeForJobToInput.value = jobFromHeader.textContent;
+
+  popupCloseButton.addEventListener('click', ()=>{
+    closePopup(popupTypeEdit);
+  });
+  popup.addEventListener('click', closeByClickOnOverlay);
   return elem.classList.add('popup_is-opened');
 };
+
+function openPopupNewCard(elem) {
+  document.addEventListener('keydown', closeWithEsc);
+  const popupContent = elem.querySelector('.popup__content')
+  const formPopup = elem.querySelector('.popup__form');
+  const popupCloseButton = popupContent.querySelector('.popup__close');
+  const placeForNameToInput = formPopup.querySelector('.popup__input_type_card-name');
+  const placeForUrlToInput = formPopup.querySelector('.popup__input_type_url');
+  popupCloseButton.addEventListener('click', ()=>{
+    closePopup(elem);
+  });
+  
+  popup.addEventListener('click', closeByClickOnOverlay);
+  
+  formPopup.addEventListener('submit', ()=>{
+    const cardData = {
+      name: placeForNameToInput.value,
+      link: placeForUrlToInput.value,
+    }
+    placesList.prepend(createCard(cardData, handleDelete))
+  });
+
+  return elem.classList.add('popup_is-opened');
+};
+
 
 function closePopup(elem) {
   document.removeEventListener('keydown', closeWithEsc);
   return elem.classList.remove('popup_is-opened');
 }
-
 function closeWithEsc(evt) {
   if (evt.key==="Escape") {
       closePopup(document.querySelector('.popup_is-opened'));
   }
 }
-
 function closeByClickOnOverlay(evt) {
   if(evt.target.classList.contains('popup_is-opened')) {
     closePopup(evt.target);
   }
 }
-
 profileEditButton.addEventListener("click", ()=>{
   openPopup (popupTypeEdit);
 });
-
-popupCloseButton.addEventListener('click', ()=>{
-  closePopup(popupTypeEdit);
-});
-
 popupTypeImage.addEventListener('click',()=>{
   openPopup(popupTypeEdit)
 })
 
-popup.addEventListener('click', closeByClickOnOverlay);
+const formElement = document.querySelector('.popup__form');
+// const nameInput = formElement.querySelector('.popup__input_type_card-name');
+// const jobInput = formElement.querySelector('.popup__input_type_description');
 
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-const nameInput = formElement.querySelector('.popup__input_type_card-name'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector('.popup__input_type_description');// Воспользуйтесь инструментом .querySelector()
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    nameFromHeader.textContent = evt.nameInput // Получите значение полей jobInput и nameInput из свойства value
-    jobFromHeader.textContent = evt.jobInput  // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    evt.preventDefault(); 
+    nameFromHeader.textContent = evt.target.children[0].value
+    jobFromHeader.textContent = evt.target.children[1].value   
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit); 
